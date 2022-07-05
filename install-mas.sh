28a5ee7cf2035c92d2526400b6fd4ed08f7223bf
dreamscached <dreamscache.d@gmail.com>
2022-07-05 11:51:59 +0200

    Debug tracing

--- install-mas.sh
+++ install-mas.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 temp="$(mktemp -d)"
 curl -L "$(curl -sL "https://api.github.com/repos/monika-after-story/monikamoddev/releases/tags/$MAS_RELEASE" | \
     perl -lne 'print $1 if /"browser_download_url": "(.+?-Mod\.zip)"/')" > "$temp/mas.zip"
