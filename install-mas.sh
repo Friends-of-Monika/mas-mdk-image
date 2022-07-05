bc2cafb29336fa2b00fc2e813bfdbfeba7b86a1c
dreamscached <dreamscache.d@gmail.com>
2022-07-05 12:04:54 +0200

    Fix ARG

--- install-mas.sh
+++ install-mas.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -x
+
 temp="$(mktemp -d)"
-curl -L "$(curl -sL "https://api.github.com/repos/monika-after-story/monikamoddev/releases/tags/$1" | \
+curl -L "$(curl -sL "https://api.github.com/repos/monika-after-story/monikamoddev/releases/tags/$MAS_RELEASE" | \
     perl -lne 'print $1 if /"browser_download_url": "(.+?-Mod\.zip)"/')" > "$temp/mas.zip"
 unzip -o "$temp/mas.zip" -d "/mdk/mas/game"
