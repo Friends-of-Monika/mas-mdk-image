4c8535e2316d02122b98a6d7607e7f3202beafb4
dreamscached <dreamscache.d@gmail.com>
2022-10-09 11:08:06 +0200

    Fix install script

--- install-mas.sh
+++ install-mas.sh
@@ -3,5 +3,6 @@
 temp="$(mktemp -d)"
 curl -L "$(curl -sL "https://api.github.com/repos/monika-after-story/monikamoddev/releases/tags/$MAS_RELEASE" | \
     perl -lne 'print $1 if /"browser_download_url": "(.+?-Mod\.zip)"/')" > "$temp/mas.zip"
-mkdir /game
-unzip -o "$temp/mas.zip" -d /game
+mkdir /tmp
+unzip -o "$temp/mas.zip" -d /tmp
+mv /tmp/game /game
